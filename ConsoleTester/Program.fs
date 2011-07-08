f5c93ae6ef1dec4dfc418eaf2fb27bff6a92bd2a
janssen.sjuul@gmail.com <janssen.sjuul@gmail.com@36fef24b-66a8-0c71-b39a-cbbb8a4b7d8e>
2011-07-08 21:36:27 +0000

    First Commit

---
+++ ConsoleTester/Program.fs
@@ -0,0 +1,18 @@
+﻿open System
+
+let x = "   
+        SELECT top 1 t1.x, fun(y), dbo.fun(y)
+        FROM t1   
+        LEFT JOIN t2
+        INNER JOIN t3 ON t3.ID = t2.ID   
+        WHERE t1.x = 50 AND y = 20    
+        ORDER BY x ASC, y DESC, z   
+    "   
+
+
+Module1.parseit x
+
+Console.ReadKey(true) |> ignore
+
+
+
---
+++ SQLParserY/Module1.fs
@@ -0,0 +1,19 @@
+﻿// Learn more about F# at http://fsharp.net
+#light
+
+module Module1
+
+open System   
+open Sql   
+ 
+
+let parseit x =
+    let lexbuf = Lexing.LexBuffer<_>.FromString x   
+    try
+        let y = SqlParser.start SqlLexer.tokenize lexbuf 
+        printfn "%A" y  
+        let z = y.RenameTables "T1" "X1"
+        printfn "%A" z
+        printfn "%A" z.toSql
+    with
+       | ex -> printfn "%A" ex
