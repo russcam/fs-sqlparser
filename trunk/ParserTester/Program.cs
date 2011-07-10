cded2e007e6f513d331f12ee329b79e84be00cc1
matt.duffield <matt.duffield@36fef24b-66a8-0c71-b39a-cbbb8a4b7d8e>
2011-07-10 02:51:38 +0000

    Added ParserTester C# console project.

---
+++ trunk/ParserTester/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ParserTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var query = @"   
+        SELECT top 1 percent blaa.t1.x, fun(y) as funny, dbo.fun(y)
+        FROM t1   
+        LEFT JOIN t2
+        INNER JOIN t3 as bla ON bla.ID = t2.ID  
+        WHERE t1.x = 50 AND y = 20    
+        ORDER BY x ASC, y DESC, z   
+    ";   
+
+
+           Sql.sqlStatement stmnt =  Module1.ParseSql(query);;
+            //Sql.ISql temp = (Sql.ISql)stmnt.Table1;
+            //temp.toSql();
+        }
+    }
+}
