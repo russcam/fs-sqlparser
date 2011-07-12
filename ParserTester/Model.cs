4b3732320ab5e6603da21211345127887ec37e73
matt.duffield <matt.duffield@36fef24b-66a8-0c71-b39a-cbbb8a4b7d8e>
2011-07-12 22:18:59 +0000

    Added Model.cs.

---
+++ ParserTester/Model.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ParserTester
+{
+    public class SqlQuery
+    {
+        #region Properties
+
+        public string Top { get; set; }
+        public List<SqlTable> Tables { get; set; }
+        public List<SqlWhere> WhereItems { get; set; }
+        public List<SqlOrderBy> OrderByColumns { get; set; }
+        public List<SqlJoin> JoinItems { get; set; }
+
+        #endregion
+
+        #region ctor
+
+        public SqlQuery()
+        {
+            this.Tables = new List<SqlTable>();
+            this.WhereItems = new List<SqlWhere>();
+            this.OrderByColumns = new List<SqlOrderBy>();
+        }
+
+        #endregion
+    };
+
+
+    public class SqlTable
+    {
+        public SqlSchema Schema { get; set; }
+        public string Alias { get; set; }
+        public string TableName { get; set; }
+        public List<SqlColumn> Columns { get; set; }
+    };
+    public class SqlSchema
+    {
+        public string SchemaName { get; set; }
+    };
+    public class SqlColumn
+    {
+        public string Alias { get; set; }
+        public string ColumnName { get; set; }
+    };
+    public class SqlWhere
+    {
+        public string LeftOperand { get; set; }
+        public string Operator { get; set; }
+        public string RightOperand { get; set; }
+    }
+    public class SqlOrderBy
+    {
+        public SqlColumn Column { get; set; }
+        public string Dir { get; set; }
+    };
+    public class SqlJoin
+    {
+        public SqlTable LeftOperand { get; set; }
+        public string SqlJoinType { get; set; }
+        public SqlTable RightOperand { get; set; }
+        public SqlWhere Criteria { get; set; }
+    };
+}
