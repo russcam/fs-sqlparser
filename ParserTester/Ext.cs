c139269f74ba34a546e60e86087840bbb550be60
janssen.sjuul@gmail.com <janssen.sjuul@gmail.com@36fef24b-66a8-0c71-b39a-cbbb8a4b7d8e>
2011-07-29 13:23:05 +0000

    Conversion from string -> AST -> Object Model -> string works :)
    Probably some edge cases left. But hey... I'm happy :P

---
+++ ParserTester/Ext.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ParserTester
+{
+    static class Ext
+    {
+        public static IEnumerable<T> ForEach<T>(this IEnumerable<T> source, Action<T> action)
+        {
+            foreach (T element in source)
+                action(element);
+            return source;
+        }
+
+        //Cant override the interface otherwise would have done that
+        public static string ToString2(this IEnumerable<object> source, string sep = ""){
+            string agg = "";
+            foreach(object element in source) {
+                agg += element.ToString() + sep;
+            }
+
+            if(agg.Length > 1 && sep.Length > 0) {
+                return agg.Substring(0, agg.Length - sep.Length);
+            } else {
+                return agg;
+            }
+        }
+    }
+
+}
--- ParserTester/Program.cs
+++ ParserTester/Program.cs
@@ -14,7 +14,7 @@ namespace ParserTester
             var query =
             @"   
                 SELECT top 1 percent blaa.t1.x, t1.q as t1q, fun(y) as funny, dbo.fun(y)
-                FROM t1   
+                FROM blaa.t1   
                 LEFT JOIN t2
                 INNER JOIN t3 as bla ON bla.ID = t2.ID  
                 WHERE t1.x = 50 AND y = 20    
@@ -25,16 +25,12 @@ namespace ParserTester
 
 
             Sql.sqlStatement stmnt = Parser.ParseSql(query);
-            SqlObject so = new SqlObject() { Top = stmnt.TopN.Value.Name, TableName = stmnt.Table1.Identifier };
-            
-            //Console.WriteLine(stmnt.Tables.Select(tbl => tbl.Name).Aggregate((tbl1,tbl2) => tbl1 + "," + tbl2));
-            Console.WriteLine(stmnt.getTableFields("t1").Select(fld => "Field:" + fld.Item1 + " FieldAlias:" + fld.Item2).Aggregate((fld1, fld2) => fld1 + "\n" + fld2));
-            var columns = stmnt.Columns.ToList();
-            foreach (var item in columns)
-            {
-                var col = item;
-                so.Columns.Add(col.Name);
-            }
+            ModelBuilder bld = new ModelBuilder();
+
+            SqlQuery qry = bld.build(query);
+            Console.Write(query + "\n\n");
+            Console.Write(qry.ToString());
+            Console.ReadKey();
         }
 
         //public static IEnumerable<TItemType> ToEnumerable<TItemType>(this FSharpList<TItemType> fList)
