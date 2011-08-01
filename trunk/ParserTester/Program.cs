25ff89d7e20d17249e90ff9dfc7c3c3d9a815392
janssen.sjuul@gmail.com <janssen.sjuul@gmail.com@36fef24b-66a8-0c71-b39a-cbbb8a4b7d8e>
2011-08-01 15:06:02 +0000

    Implemented merging query's

--- trunk/ParserTester/Program.cs
+++ trunk/ParserTester/Program.cs
@@ -13,7 +13,7 @@ namespace ParserTester
         {
             var query =
             @"   
-                SELECT top 1 percent blaa.t1.x, t1.q as t1q, fun(y) as funny, dbo.fun(y)
+                SELECT top 1 percent blaa.t1.x, t1.q as t1q, fun(y) as funny
                 FROM blaa.t1   
                 LEFT JOIN t2
                 INNER JOIN t3 as bla ON bla.ID = t2.ID  
@@ -21,22 +21,26 @@ namespace ParserTester
                 ORDER BY x ASC, y DESC, z   
             ";
 
+            var query2 =
+            @"   
+                SELECT dbo.fun(y)
+                FROM someTbl
+            ";
+
             // YOU SHOULD TRY RIGHTCLICK ON A F# FUNCTION AND THEN GO TO DEFINITION   -> WOW WTF
 
 
             Sql.sqlStatement stmnt = Parser.ParseSql(query);
-            ModelBuilder bld = new ModelBuilder();
 
-            SqlQuery qry = bld.build(query);
-            Console.Write(query + "\n\n");
+            SqlQuery qry = ModelBuilder.build(query);
+            Console.WriteLine(query + "\n\n");
+            Console.WriteLine("\n----------------");
+            Console.WriteLine(qry.ToString());
+            Console.WriteLine("\n----------------");
+            qry = ModelBuilder.append(qry, query2);
             Console.Write(qry.ToString());
             Console.ReadKey();
         }
-
-        //public static IEnumerable<TItemType> ToEnumerable<TItemType>(this FSharpList<TItemType> fList)
-        //{
-        //    return Microsoft.FSharp.Collections.SeqModule.OfList<TItemType>(fList);
-        //}
     };
 
     public class SqlObject
